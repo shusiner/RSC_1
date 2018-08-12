4a1dcb0821068582623e5bbf62f864b93987dc4f
shusiner <quantum.plato@gmail.com>
2018-08-13 04:11:54 +0800

    Added files and changes required for deployment to heroku

--- rsc2/settings.py
+++ rsc2/settings.py
@@ -38,7 +38,7 @@ DATABASES = {
 # DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['sheltered-harbor-71913.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
